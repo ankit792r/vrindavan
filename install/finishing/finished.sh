7346af56e381981342822a49b32f2e32800c3586
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-12-19 13:30:43 +0530

    fixed exit script

--- install/finishing/finished.sh
+++ install/finishing/finished.sh
@@ -1,22 +1,3 @@
-stop_install_log
-
-echo_in_style() {
-  echo "$1" | tte --canvas-width 0 --anchor-text c --frame-rate 640 print
-}
-
-clear
-
-# Display installation time if available
-if [[ -f $VRINDAVAN_INSTALL_LOG_FILE ]] && grep -q "Total:" "$VRINDAVAN_INSTALL_LOG_FILE" 2>/dev/null; then
-  echo
-  TOTAL_TIME=$(tail -n 20 "$VRINDAVAN_INSTALL_LOG_FILE" | grep "^Total:" | sed 's/^Total:[[:space:]]*//')
-  if [ -n "$TOTAL_TIME" ]; then
-    echo_in_style "Installed in $TOTAL_TIME"
-  fi
-else
-  echo_in_style "Finished installing"
-fi
-
 # Exit gracefully if user chooses not to reboot
 if gum confirm --padding "0 0 0 $((PADDING_LEFT + 32))" --show-help=false --default --affirmative "Reboot Now" --negative "" ""; then
   # Clear screen to hide any shutdown messages
