993e9ce6a6a66068b306a9224b2b57caeb2fa846
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-12-19 13:33:38 +0530

    fixed exit script

--- install/finishing/finished.sh
+++ install/finishing/finished.sh
@@ -1,7 +1,2 @@
-# Exit gracefully if user chooses not to reboot
-if gum confirm --padding "0 0 0 $((PADDING_LEFT + 32))" --show-help=false --default --affirmative "Reboot Now" --negative "" ""; then
-  # Clear screen to hide any shutdown messages
-  clear
-
-  sudo reboot 2>/dev/null
-fi
+echo "Rebooting..."
+sudo reboot
